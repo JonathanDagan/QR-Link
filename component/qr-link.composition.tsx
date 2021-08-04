5e5bfeaa4208b975d372e77bec4f42fb6cf0593d
Jonathan Dagan <jonathandagan12@gmail.com>
2021-08-04 23:28:34 +0300

    0.1.0 release

--- component/qr-link.composition.tsx
+++ component/qr-link.composition.tsx
@@ -2,5 +2,5 @@ import React from 'react';
 import { QrLink } from './qr-link';
 
 export const UrlGeneratedQrlink = () => (
-  <QrLink url="http://facebook.github.io/react/" />
+  <QrLink url="https://bit.dev/jonathandagan/utils"/>
 );
--- component/qr-link.spec.tsx
+++ component/qr-link.spec.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { UrlGeneratedQrlink } from './qr-link.composition';
 
-const ReactLink = "http://facebook.github.io/react/";
+const ComponentLibraryLink = "https://bit.dev/jonathandagan/utils";
 
 it('should render with the correct link', () => {
   const { getByTestId } = render(<UrlGeneratedQrlink />);
   const rendered = getByTestId("qr-link");
-  expect(rendered.firstElementChild).toHaveAttribute('href', ReactLink)
+  expect(rendered.firstElementChild).toHaveAttribute('href', ComponentLibraryLink)
 });
